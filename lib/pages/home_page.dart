61ade0485502f2af0b1e05b5b52e8cd13ae67876
adityasnr <adityasnr160@gmail.com>
2022-10-11 06:41:43 +0530

    validator added

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,8 +1,11 @@
+
+// ignore_for_file: use_key_in_widget_constructors, prefer_const_constructors, sort_child_properties_last, duplicate_ignore
+
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
   final String days = "Sun";
-  
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -10,116 +13,107 @@ class HomePage extends StatelessWidget {
         title: Text("Hydra eSupport"),
       ),
       body: Center(
+        // ignore: avoid_unnecessary_containers, duplicate_ignore
         child: Container(
           // ignore: prefer_const_constructors
           child: Column(
             children: [
-
               SizedBox(
+                height: 40.0,
+              ),
+              Column(
+                children: [
+                  ////first container
+                  Container(
+                    width: 300,
+                    height: 300,
+                    alignment: Alignment.center,
+                    child: Text(
+                      "Box 1",
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18),
+                    ),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(10.0),
+                        topRight: Radius.circular(10.0),
+                        bottomLeft: Radius.circular(10.0),
+                        bottomRight: Radius.circular(10.0),
+                      ),
+                      // ignore: prefer_const_literals_to_create_immutables
+                      boxShadow: [
+                        BoxShadow(
+                          color: Color.fromARGB(255, 172, 169, 169),
+                          offset: const Offset(
+                            5.0,
+                            5.0,
+                          ),
+                          blurRadius: 10.0,
+                          spreadRadius: 2.0,
+                        ), //BoxShadow
+                        BoxShadow(
+                          color: Colors.white,
+                          offset: const Offset(0.0, 0.0),
+                          blurRadius: 0.0,
+                          spreadRadius: 0.0,
+                        ), //BoxShadow
+                      ],
+                    ),
+                  ),
+                  //// Spacing between
+                  SizedBox(
                     height: 40.0,
                   ),
-
-            Column(
-              
-              children: [
-////first container
-                Container(
-                          width: 300,
-                          height: 300,
-                          alignment: Alignment.center,
-                          child: Text(
-                            "Box 1",
-                            style: TextStyle(
-                                color: Colors.black,
-                                fontWeight: FontWeight.bold,
-                                fontSize: 18),
-                          ),
-                          decoration: BoxDecoration(
-                              color: Colors.white,
-                            
-                              borderRadius: BorderRadius.only(
-                               topLeft: Radius.circular(10.0),
-                               topRight: Radius.circular(10.0),
-                               bottomLeft: Radius.circular(10.0),
-                               bottomRight: Radius.circular(10.0), ),
-
-                              
-                              boxShadow: [
-                                  BoxShadow(
-                                     color: Color.fromARGB(255, 172, 169, 169),
-                                     offset: const Offset(
-                                       5.0,
-                                       5.0,
-                                     ),
-                                     blurRadius: 10.0,
-                                     spreadRadius: 2.0,
-                                   ), //BoxShadow
-                                   BoxShadow(
-                                     color: Colors.white,
-                                     offset: const Offset(0.0, 0.0),
-                                     blurRadius: 0.0,
-                                     spreadRadius: 0.0,
-                                   ), //BoxShadow
-                             ],
-
-                               ),
-                        ),
-//// Spacing between
-                SizedBox(
-                       height: 40.0,
+                  //// second container
+                  Container(
+                    width: 300,
+                    height: 300,
+                    alignment: Alignment.center,
+                    child: Text(
+                      "Box 2",
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18),
+                    ),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(10.0),
+                        topRight: Radius.circular(10.0),
+                        bottomLeft: Radius.circular(10.0),
+                        bottomRight: Radius.circular(10.0),
                       ),
-//// second container
-                Container(
-                          width: 300,
-                          height: 300,
-                          alignment: Alignment.center,
-                          child: Text(
-                            "Box 2",
-                            style: TextStyle(
-                                color: Colors.black,
-                                fontWeight: FontWeight.bold,
-                                fontSize: 18),
+                      // ignore: prefer_const_literals_to_create_immutables
+                      boxShadow: [
+                        BoxShadow(
+                          color: Color.fromARGB(255, 172, 169, 169),
+                          offset: const Offset(
+                            5.0,
+                            5.0,
                           ),
-                          decoration: BoxDecoration(
-                              color: Colors.white,
-                            
-                              borderRadius: BorderRadius.only(
-                               topLeft: Radius.circular(10.0),
-                               topRight: Radius.circular(10.0),
-                               bottomLeft: Radius.circular(10.0),
-                               bottomRight: Radius.circular(10.0), ),
-
-                              
-                              boxShadow: [
-                                  BoxShadow(
-                                     color: Color.fromARGB(255, 172, 169, 169),
-                                     offset: const Offset(
-                                       5.0,
-                                       5.0,
-                                     ),
-                                     blurRadius: 10.0,
-                                     spreadRadius: 2.0,
-                                   ), //BoxShadow
-                                   BoxShadow(
-                                     color: Colors.white,
-                                     offset: const Offset(0.0, 0.0),
-                                     blurRadius: 0.0,
-                                     spreadRadius: 0.0,
-                                   ), //BoxShadow
-                             ],
-
-                               ),
-                        ),
-              ],
-            ),
+                          blurRadius: 10.0,
+                          spreadRadius: 2.0,
+                        ), //BoxShadow
+                        BoxShadow(
+                          color: Colors.white,
+                          offset: const Offset(0.0, 0.0),
+                          blurRadius: 0.0,
+                          spreadRadius: 0.0,
+                        ), //BoxShadow
+                      ],
+                    ),
+                  ),
+                ],
+              ),
             ],
           ),
         ),
       ),
-
-
       drawer: Drawer(),
     );
-
   }
 }
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,119 +1,130 @@
+// ignore_for_file: prefer_const_constructors, use_key_in_widget_constructors, unused_import, use_build_context_synchronously
 import 'dart:ffi';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_application_2/pages/utils/routes.dart';
 
-// ignore: use_key_in_widget_constructors
 class LoginPage extends StatefulWidget {
   @override
   State<LoginPage> createState() => _LoginPageState();
 }
 
-String name = '';
-bool changeButton = false;
-
 class _LoginPageState extends State<LoginPage> {
+  String name = '';
+  bool changeButton = false;
+
+  final _formkey = GlobalKey<FormState>();
+
+  moveToHome(BuildContext context) async {
+    if (_formkey.currentState!.validate()) {
+      setState(() {
+        changeButton = true;
+      });
+      await Future.delayed(Duration(seconds: 1));
+      await Navigator.pushNamed(context, MyRoutes.homeRoute);
+      setState(() {
+        changeButton = false;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Material(
         color: Colors.white,
-        child: Column(
-          children: [
-            SizedBox(
-              height: 50.0,
-            ),
-
-            Image.asset(
-              "assets/images/welcome.png",
-              fit: BoxFit.cover,
-            ),
-
-            // ignore: prefer_const_constructors
-            SizedBox(
-              height: 20.0,
-            ),
-
-            // ignore: prefer_const_constructors
-            Text(
-              "Welcome $name",
-              style: const TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-
-            Padding(
-              padding:
-                  const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
-              child: Column(
-                children: [
-                  TextFormField(
-                    // ignore: prefer_const_constructors
-                    decoration: InputDecoration(
-                      hintText: "Enter Username",
-                      labelText: "Username",
-                    ),
-                    onChanged: ((value) {
-                      name = value;
-                      setState(() {});
-                    }),
+        child: SingleChildScrollView(
+          child: Form(
+            key: _formkey,
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 50.0,
+                ),
+                Image.asset(
+                  "assets/images/welcome.png",
+                  fit: BoxFit.cover,
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                Text(
+                  "Welcome $name",
+                  style: const TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
                   ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                      vertical: 16.0, horizontal: 32.0),
+                  child: Column(
+                    children: [
+                      TextFormField(
+                        decoration: InputDecoration(
+                          hintText: "Enter Username",
+                          labelText: "Username",
+                        ),
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return "Username cannot be empty";
+                          }
+                          return null;
+                        },
+                        onChanged: ((value) {
+                          name = value;
+                          setState(() {});
+                        }),
+                      ),
 
-                  TextFormField(
-                    // ignore: prefer_const_constructors
-                    obscureText: true,
-                    // ignore: prefer_const_constructors
-                    decoration: InputDecoration(
-                      hintText: "Enter Password",
-                      labelText: "Password",
-                    ),
+                      TextFormField(
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          hintText: "Enter Password",
+                          labelText: "Password",
+                        ),
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return "Password cannot be empty";
+                          } else if (value.length < 6) {
+                            return "password length should be minimun 6";
+                          }
+                          return null;
+                        },
+                      ),
+
+                      SizedBox(
+                        height: 40.0,
+                      ),
+
+                      InkWell(
+                        onTap: () => moveToHome(context),
+                        child: AnimatedContainer(
+                          duration: Duration(seconds: 1),
+                          width: changeButton ? 50 : 150,
+                          height: 50,
+                          alignment: Alignment.center,
+                          // ignore: sort_child_properties_last
+                          child: changeButton
+                              ? Icon(Icons.done, color: Colors.white)
+                              : Text(
+                                  "Login",
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 18),
+                                ),
+                          decoration: BoxDecoration(
+                              color: Colors.deepPurple,
+                              borderRadius:
+                                  BorderRadius.circular(changeButton ? 50 : 8)),
+                        ),
+                      ),
+            
+                    ],
                   ),
-
-                  // ignore: prefer_const_constructors
-                  SizedBox(
-                    height: 40.0,
-                  ),
-
-                  InkWell(
-                    onTap: (() async {
-                      setState(() {
-                        changeButton = true;
-                      });
-                      await Future.delayed(Duration(seconds: 1));
-                      Navigator.pushNamed(context, MyRoutes.homeRoute);
-                    }),
-                    child: AnimatedContainer(
-                      duration: Duration(seconds: 1),
-                      width: changeButton ? 50 : 150,
-                      height: 50,
-                      alignment: Alignment.center,
-                      child: changeButton
-                          ? Icon(Icons.done, color: Colors.white)
-                          : Text(
-                              "Login",
-                              style: TextStyle(
-                                  color: Colors.white,
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 18),
-                            ),
-                      decoration: BoxDecoration(
-                          color: Colors.deepPurple,
-                          borderRadius:
-                              BorderRadius.circular(changeButton ? 50 : 8)),
-                    ),
-                  ),
-
-                  // ElevatedButton(
-                  //   child: Text("Login"),
-                  //   style: TextButton.styleFrom(),
-                  //   onPressed: () {
-                  //     Navigator.pushNamed(context, MyRoutes.homeRoute);
-                  //   },
-                  // ),
-                ],
-              ),
-            )
-          ],
+                )
+              ],
+            ),
+          ),
         ));
   }
 }
